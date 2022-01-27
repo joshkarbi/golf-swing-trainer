e57d916f3d465754c0e03beebc7eebf76bca6938
Josh Karbi <jkarbi3@uwo.ca>
2022-01-27 11:03:25 -0500

    Marking video frame timestamps in data extraction

--- backend/data_extraction/video_analysis.py
+++ backend/data_extraction/video_analysis.py
@@ -1,5 +1,5 @@
 
-from typing import NamedTuple, Generator, Any
+from typing import NamedTuple, Generator, Any, Tuple
 
 import cv2
 import pandas as pd
@@ -10,6 +10,8 @@ class GolfSwingVideoFrameInfo(NamedTuple):
     """Info from 1 frame in a golf swing video.
     All coordinates / dimensions are in pixels.
     """
+    timestamp: float
+
     video_width: int
     video_height: int
 
@@ -17,8 +19,9 @@ class GolfSwingVideoFrameInfo(NamedTuple):
     ball_y: int
 
 Image = Any
-def get_video_frames(video_file_name: str) -> Generator[Image, None, None]:
-    """Generator to pull image frames out of a video file.
+def get_video_frames(video_file_name: str) -> Generator[Tuple[Image, float], None, None]:
+    """Generator to pull image frames and 
+    timestamps out of a video file.
 
     Parameters
     ----------
@@ -27,11 +30,14 @@ def get_video_frames(video_file_name: str) -> Generator[Image, None, None]:
     """
 
     vs = cv2.VideoCapture(video_file_name)
+    frames_per_second = vs.get(cv2.CAP_PROP_FPS)
+    timestamp = 0
 
     while True:
         is_next_frame, frame = vs.read()
         if is_next_frame:
-            yield frame
+            yield [frame, round(timestamp, 2)]
+            timestamp += 1/frames_per_second 
         else:
             return
             
@@ -48,7 +54,7 @@ def analyze_video(video_file_name: str) -> pd.DataFrame:
     observations = []
     prev_ball_x, prev_ball_y = None, None
 
-    for image in get_video_frames(video_file_name=video_file_name):
+    for image, timestamp in get_video_frames(video_file_name=video_file_name):
 
         height, width = image.shape[0], image.shape[1]
 
@@ -61,6 +67,7 @@ def analyze_video(video_file_name: str) -> pd.DataFrame:
 
         observations.append(
             GolfSwingVideoFrameInfo(
+                timestamp=timestamp,
                 video_width=width,
                 video_height=height,
                 ball_x=ball_x,
