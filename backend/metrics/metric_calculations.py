af01a189394f9481f657eda589d9f151bc25fb98
olamumot <46768039+olamumot@users.noreply.github.com>
2022-02-09 15:09:52 -0500

    metrics using new dummy data, splitting feedback

--- backend/metrics/metric_calculations.py
+++ backend/metrics/metric_calculations.py
@@ -1,66 +1,118 @@
 '''
 Use image detection to generate Dataframe of corresponding data points. Ball, club, body positions will be tracked. 
 This file will generate some dummy data for these positions and perform the calculations to track metrics.
+
+    X and Y coordinates are distances from top left corner of the image
+    going rightward and downward, respectively, in pixels.
+
 '''
 
-## dummy data for ball position with timestamp 
 import pandas as pd
-ball_position_data = [
-    [0.0, 405, 532, 'NULL', 'NULL'],
-    [0.03, 405, 532, 'NULL', 'NULL'],
-    [0.06, 405, 532, 'NULL', 'NULL'],
-    [0.09, 405, 532, 'NULL', 'NULL'],
-    [0.12, 405, 532, 171.536, 441.05],
-    [0.15, 405, 532, 171.536, 441.05],
-    [0.18, 405, 532, 171.536, 441.05],
-    [0.84, 405, 532, 171.536, 441.05],
-    [0.87, 405, 532, 171.536, 441.05],
-    [0.9, 405, 532, 171.536, 441.05],
-    [0.30, 405, 532, 171.536, 441.05],
-    [1.08, 405, 532, 171.536, 441.05],
-    [1.11, 405, 532, 171.536, 441.05],
-    [1.14, 405, 532, 271.68, 376.94],
-    [1.17, 405, 532, 271.68, 376.94],
-    [1.2, 405, 532, 344.42, 330.02],
-    [1.23, 405, 532, 344.42, 330.02],
-    [1.26, 405, 532, 344.42, 330.02],
-    [1.29, 405, 532, 344.42, 330.02],
-    [1.32, 405, 532, 344.42, 330.02],
-    [1.35, 405, 532, 344.42, 330.02],
-    [1.38, 405, 532, 344.42, 330.02]
-]
+from tomlkit import string
 
-ball_pos_df = pd.DataFrame(ball_position_data, columns= ['timestamp', 'video_width', 'video_height', 'ball_x', 'ball_y'])
-#print(ball_pos_df)
+class GolfSwingFeedbackInfoAndMetrics(NamedTuple):
+    ball_speed: int
+    launch_angle: int
 
-# calculate ball speed
+    '''Body Position Feedback:
+        each speicific body position will have a true/false for accuracy success
+        as well as written feedback for what to change 
+        ex. arm position success is false, tuck elbows in
+        when returning feedback to user, only messages corresponding to false success will appear
+    '''
+    shoulder_feet_pos_success: bool
+    shoulder_feet_pos_feedback: string
 
-# calculate launch angle (just gonna assume ground y position is 20 pixels lower than ball position)
+    arm_pos_success: bool
+    arm_pos_feedback: string
 
-body_pos_df = pd.read_csv ('body_position_data.csv')
-#print(body_pos_df)
+vid_analysis_df = pd.read_csv('debug_data_extraction.csv')
 
-# check if position of feet is within a certain range of shoulder position 
-right_shoulder_x = body_pos_df.loc[:, 'right_shoulder_x']
-left = body_pos_df.loc[:, 'left_shoulder_x']
-
-right_ankle_x = body_pos_df.loc[:, 'right_ankle_x']
-left_ankle_x = body_pos_df.loc[:, 'left_ankle_x']
+def analyze_datapoints(vid_analysis_df) -> pd.DataFrame:
+    '''
+    Input is the csv generated from video analysis,
+    output will result in df including metrics, and feedback for body positions 
+    TODO: score shot based on quality of feedback acchieved 
+    '''
+    feedback = []
+    ball_speed_metrics = []
+    shoulder_feet_pos_metrics = []
+    arm_pos_metrics = []
 
 
-# are x and y values switched for this? data doesnt make much sense, getting negatives
-for index, row in body_pos_df.iterrows():
-    right_shoulder_x = row['right_shoulder_x']
-    left_shoulder_x = row['left_shoulder_x']
-    right_ankle_x = row['right_ankle_x']
-    left_ankle_x = row['left_ankle_x']
+    for index, row in vid_analysis_df.iterrows():
 
-    shoulder_diff = right_shoulder_x - left_shoulder_x
-    ankle_diff = right_ankle_x - left_ankle_x
-    print(shoulder_diff, ankle_diff)
+        '''Ball Speed Calculations 
+            ball speed corresponds to the speed of the ball after impact
+            ball_x, ball_y might be NULL for the first few timestamps, 
+            need to determine original position of ball (account for noise)
+            as well as when ball starts drastically changing position (in flight)
+            gather first few positions and timetamps of ball in impact and estimate speed
+            TODO: actual calc for ball speed
+        '''
 
-    #print(row['right_shoulder_x'], row['left_shoulder_x'])
+        #append if not null
+        ball_speed_metrics.append([row['timestamp'], row['ball_x'],row['ball_y']])
+
+        #once enough points are captured to determine speed, call function
+        ball_speed = ball_speed_calculation(ball_speed_metrics)
 
 
+        '''Launch Angle Calculation
+            angle of ball relative to ground after its been hit
+            for now just estimate ground position (x amount of pixels lower than ball)
+            TODO: actual calc for ball speed
+        '''
+        ground_pos_y = row['ball_y'] - 20
+        launch_angle = launch_angle_calculation(ball_speed, ground_pos_y)
 
 
+        '''Feet/Shoulder Position Feedback
+            four types of feet stances, each corresponds to different shot type:
+                - wide (inside of feet align with outside of shoulders)
+                - normal (feet and shoulder width equal distance)
+                - narrow (outside of feet align with outside of shoulder)
+                - very narrow (feet almost toching)
+
+            check if position of feet is outside a range of shoulder position, can provide feedback if feet are too wide
+            ref: https://www.golfdistillery.com/swing-tips/setup-address/feet-position/
+        '''
+
+        #append position before shot, timestamp of when ball starts moving will be the final metric we need here
+        shoulder_feet_pos_metrics.append([row['timestamp'], row['right_shoulder_x'], row['left_shoulder_x'], row['right_ankle_x'], row['left_ankle_x']])
+
+        shoulder_feet_pos_feedback = shoulder_feet_pos_feedback(shoulder_feet_pos_metrics)
+    
+        '''Arm Position Feedback
+            Determine accuracy of arm position (the leading arm in the shot)
+            Four main sections:
+                - address
+                - backswing
+                - downswing
+                - followthrough
+            I guess it's necessary to keep track of most of these positions as the entire swing itself is a constant moevement 
+            ref: https://theleftrough.com/right-arm-in-golf-swing/
+        '''
+
+        arm_pos_metrics.append([row['timestamp'], row['left_shoulder_x'], row['left_shoulder_y'], row['right_shoulder_x'], row['right_shoulder_y'], 
+            row['left_elbow_x'], row['left_elbow_y'], row['right_elbow_x'], row['right_elbow_y'],
+            row['left_wrist_x'], row['left_wrist_y'], row['right_wrist_x'], row['right_wrist_y']])
+        
+        #call this once we have all the arm position metrics we need, just adding here as reference 
+        arm_pos_feedback = arm_pos_feedback(arm_pos_metrics)
+
+def ball_speed_calculation(ball_speed_data):
+    '''return ball speed '''
+
+def launch_angle_calculation(ball_speed, ground_pos):
+    '''return launch angle based on ball speed '''
+
+def shoulder_feet_pos_feedback(pos_data): 
+    '''Determine if feet position is satisfactory, return tailored feedback message if not 
+        shoulder_diff = right_shoulder_x - left_shoulder_x
+        ankle_diff = right_ankle_x - left_ankle_x
+    
+    '''
+
+def arm_pos_feedback(pos_data):
+    '''Determine if arm position is satisfactory, return tailored feedback message if not '''
